43a2e9a334ae5ff0256fb945deb6e20b44ac0585
victorlee0505 <viclee@my.yorku.ca>
2018-11-28 21:53:10 -0500

    finalized

--- Project2/threeA.sql
+++
@@ -1,9 +0,0 @@
-connect to STL;
-
-select P.cust#, sum(P.qnty)
-from stl.Purchase P, stl.Customer C, stl.Book B
-where P.cust# = C.cust# and C.cust# = 1
-group by P.cust#;
-
-connect reset;
-terminate;
